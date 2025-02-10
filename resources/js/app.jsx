7822a99937427bf9fd3fdb8fcb3bf4fca3c8c72c
vatoer <vatoer.ckplus@gmail.com>
2025-02-10 16:01:36 +0100

    progress indicator

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -15,4 +15,10 @@ createInertiaApp({
   setup({ el, App, props }) {
     createRoot(el).render(<App {...props} />)
   },
+
+  progress: {
+   color: '#f00',
+  }
+
+
 })
--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,6 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
+    sleep(2);
     return Inertia::render('Home',['name' => 'John Doe']);
 });
