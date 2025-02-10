1bed81417e727fe43348cc0b5860ede77057a89b
vatoer <vatoer.ckplus@gmail.com>
2025-02-10 15:47:25 +0100

    add Layout

---
+++ resources/js/Layouts/Layout.jsx
@@ -0,0 +1,15 @@
+export default function Layout({ children }) {
+    return(
+        <>
+        <header>
+            <nav>
+                <a className="nav-link" href="/">Home</a>
+                <a className="nav-link" href="/create">Create</a>
+            </nav>
+        </header>
+        <main>  
+            {children}
+        </main>
+        </>
+    )
+}
--- resources/js/Pages/home.jsx
+++ resources/js/Pages/home.jsx
@@ -1,7 +1,13 @@
-export default function Home({name}) {
+import Layout from "../Layouts/Layout";
+
+function Home({name}) {
     return (
         <div>
             <h1 className="title">Hello, {name}</h1>
         </div>
     );
 }
+
+Home.layout = page => <Layout children={page} />;
+
+export default Home;
