96dc6a07d986b96d83c070df77495ccedbd5ad17
vatoer <vatoer.ckplus@gmail.com>
2025-02-10 15:57:32 +0100

    preserve scroll

--- resources/js/Pages/home.jsx
+++ resources/js/Pages/home.jsx
@@ -1,7 +1,11 @@
+import { Link } from "@inertiajs/react";
+
 export default function Home({name}) {
     return (
         <div>
             <h1 className="title">Hello, {name}</h1>
+            <Link preserveScroll  href="/" className="block title mt-[1000px]"> {new Date().toLocaleTimeString() }
+            </Link>
         </div>
     );
 }
