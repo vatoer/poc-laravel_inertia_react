94cefdb591a6edd825930ff1d56d5b9ec78a6b14
git uploader <abrary@basic41.id>
2025-02-10 22:08:16 +0100

    delete resource

--- resources/js/Pages/Show.jsx
+++ resources/js/Pages/Show.jsx
@@ -1,5 +1,19 @@
-import { Link } from "@inertiajs/react";
+import { Link, useForm } from "@inertiajs/react";
+import {useRoute} from "../../../vendor/tightenco/ziggy"
+
 export default function Show({ post }) {
+
+    const { delete: destroy } = useForm();
+    const route = useRoute();
+
+
+    function submit(e) {
+        e.preventDefault();
+        // destroy(`/posts/${post.id}`);
+        destroy(route('posts.destroy', post));
+    }
+
+    console.log(useForm());
     return (
         <>
             <div className="p-4 flex flex-col border-b border-gray-100">
@@ -12,6 +26,12 @@ export default function Show({ post }) {
                 <Link className="text-link" href={`/posts/${post.id}/edit`}>
                     Edit
                 </Link>
+
+                <div className="flex items-center justify-end gap-2">
+                    <form onSubmit={submit}>
+                        <button className="bg-red-500 text-slate-100 px-4 py-1  rounded">Delete</button>
+                    </form>
+                </div>
             </div>
         </>
     );ß
